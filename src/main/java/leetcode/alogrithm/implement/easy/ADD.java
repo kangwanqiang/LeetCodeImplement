6999ff3d312d4562192a6a52164dcad7f75754ce
kangwanqiang <766574872@qq.com>
2021-01-31 20:37:34 +0800

    project init

---
+++ src/main/java/leetcode/alogrithm/implement/easy/ADD.java
@@ -0,0 +1,11 @@
+package leetcode.alogrithm.implement.easy;
+
+/**
+ * @author kangwanqiang
+ * @date 2021/1/31
+ */
+public class ADD {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
